0234515c3535fc787b8ce1bb0ca9e091d12d977a
gudziv <gudz_iv@mail.ru>
2025-12-02 20:29:14 +0300

    Знакомство с gRPC протоколом

---
+++ xml_python.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+
+
+xml_data = """
+<user>
+    <id>1</id>
+    <first_name>John</first_name>
+    <last_name>Doe</last_name>
+    <email>john.doe@example.com</email>
+</user>
+"""
+
+root = ET.fromstring(xml_data)
+
+print("User ID", root.find('id').text)
+print("User First name", root.find('first_name').text)
+print("User Last name", root.find('last_name').text)
+print("User Email", root.find('email').text)
+
+
